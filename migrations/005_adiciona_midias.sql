730a5ea6adf7bf90340030f55d8dc88c75896e0e
João Emannuel <83120021+emannoel@users.noreply.github.com>
2026-07-09 09:22:28 -0300

    Update data_upload column in midias table
    Removed default value for data_upload column.

--- migrations/005_adiciona_midias.sql
+++ migrations/005_adiciona_midias.sql
@@ -5,5 +5,5 @@ CREATE TABLE midias (
     id_tweet        INTEGER NOT NULL REFERENCES tweets(id_tweet) ON DELETE CASCADE,
     tipo_midia      VARCHAR(20) NOT NULL CHECK (tipo_midia IN ('imagem', 'video', 'gif')),
     url_midia       VARCHAR(255) NOT NULL,
-    data_upload     TIMESTAMP NOT NULL DEFAULT NOW()
+    data_upload     TIMESTAMP NOT NULL 
 );
